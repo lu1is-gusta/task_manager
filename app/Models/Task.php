273d25e715e405098c526bc24a0eb5a308154c74
Luís Gustavo <gustavovacodar@gmail.com>
2023-07-25 12:05:01 -0300

    create migrations and models

---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable  = [
+        'title',
+        'description',
+        'date',
+        'category_id',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
