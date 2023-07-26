a911e972cff3d35ddc9dd9183c1ce36732d51e61
Luís Gustavo <gustavovacodar@gmail.com>
2023-07-26 11:29:55 -0300

    structure basic templates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task Manager</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.layout')
+@section('title', 'Home Page')
+@section('content')
+    <div class="container">
+    <div class="sidebar">
+        <div class="content">
+            <nav>
+                <a href="#" class="btn btn-primary">Create Task</a>
+            </nav>
+            <main>
+                <section class="graph">
+                    <div class="graph-header">
+                        <h2>Progress of the Day</h2>
+                        <hr class="lineheader">
+                        Date
+                    </div>
+                    <div class="graph-header-subtitle"> Tasks: <b>3/6</b></div>
+
+                    <div class="graph-placeholder"></div>
+                    <p class="graph-header-tasks-left"></p>
+                </section>
+                <section class="list">
+                    <div class="list-header">
+                        <select name="" id="" class="list-header-select">
+                            <option value="1"> All tasks</option>
+                        </select>
+                    </div>
+                </section>
+            </main>
+        </div>
+    </div>
+    </div>
+@stop
